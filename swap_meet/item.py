01857c213dfd4a2a14fb5b870f54a25b21deacaa
shiyinxi <shiyinxi@gmail.com>
2024-09-30 16:37:47 -0700

    wave 03 completed with tests passed

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -6,3 +6,6 @@ class Item:
 
     def get_category(self):
         return type(self).__name__
+    
+    def __str__(self):
+        return f"An object of type Item with id {self.id}."
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,3 +1,4 @@
+# from .item import Item
 class Vendor:
     def __init__(self, inventory=None):
         self.inventory = [] if inventory is None else inventory
@@ -17,3 +18,13 @@ class Vendor:
             if item.id == id:
                 return item     
         return None
+    
+    def swap_items(self, other_vendor, my_item, their_item):
+        if my_item not in self.inventory or their_item not in other_vendor.inventory:
+            return False
+        
+        self.inventory.remove(my_item)
+        self.inventory.append(their_item)
+        other_vendor.inventory.remove(their_item)
+        other_vendor.inventory.append(my_item)
+        return True
