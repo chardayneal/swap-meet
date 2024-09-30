34432fa891715b7bbcca6a293ca23e2e66dd1c23
Charday Neal <charday.neal@gmail.com>
2024-09-30 12:56:19 -0700

    Complete wave 2 tests

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,4 +1,8 @@
 from uuid import uuid4
 
 class Item:
-    pass
+    def __init__(self, id=None):
+        self.id = uuid4().int if id is None else id
+
+    def get_category(self):
+        return type(self).__name__
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -11,3 +11,9 @@ class Vendor:
             return False
         self.inventory.remove(item)
         return item
+
+    def get_by_id(self, id):
+        for item in self.inventory:
+            if item.id == id:
+                return item     
+        return None
