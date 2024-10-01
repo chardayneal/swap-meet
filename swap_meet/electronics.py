638d19f0e0067fb4be19f38bb9ed3c478bf8a55f
shiyinxi <shiyinxi@gmail.com>
2024-10-01 15:55:06 -0700

    fix wave05

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,12 +1,12 @@
 from swap_meet.item import Item
 class Electronics(Item):
     def __init__(self, id=None, type="Unknown",condition=0):
-        super().__init__(id=None,condition=0)
+        super().__init__(id,condition)
         self.type = type
-        if not id:
-            super().__init__()
-        else:
-            self.id = id
+        # if not id:
+        #     super().__init__()
+        # else:
+        #     self.id = id
 
     def __str__(self):
         return f"An object of type Electronics with id {self.id}. This is a {self.type} device."
